ac93a9f7ef186508d055383af369d30e61b1f9bb
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-11 19:35:49 -0200

    Correção para projeto buildar

--- GeekBurger.Dashboard/Startup.cs
+++ GeekBurger.Dashboard/Startup.cs
@@ -24,14 +24,14 @@ namespace GeekBurger.Dashboard
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app,
+                              IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run();
+            app.UseMvc();
         }
     }
 }
