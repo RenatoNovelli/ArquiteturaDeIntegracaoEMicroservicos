35285d2572a61c1523a45c60e4f3d2594b8fab15
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-18 19:59:49 -0300

    Service Bus

---
+++ GeekBurger.Dashboard/Interfaces/Service/IReceiveMessagesFactory.cs
@@ -0,0 +1,9 @@
+﻿using GeekBurger.Dashboard.Services;
+
+namespace GeekBurger.Dashboard.Interfaces.Service
+{
+    public interface IReceiveMessagesFactory
+    {
+        ReceiveMessagesService CreateNew(string topic, string subscription, string filterName = null, string filter = null);
+    }
+}
---
+++ GeekBurger.Dashboard/Services/ReceiveMessagesFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace GeekBurger.Dashboard.Services
+{
+    public class ReceiveMessagesFactory
+    {
+        public ReceiveMessagesFactory()
+        {
+            //By default, creates this receivemessageservice
+            CreateNew("uicommand", "html");
+            CreateNew("orderpaid", "html", "filter-store", "8048e9ec-80fe-4bad-bc2a-e4f4a75c834e");
+        }
+
+        public ReceiveMessagesService CreateNew(string topic, string subscription, string filterName = null, string filter = null)
+        {
+            return new ReceiveMessagesService(topic, subscription, filterName, filter);
+        }
+    }
+}
